2b2481ab591c5605a8b7dd160b5a9a4bffc56302
yujinhong01 <yujinhong01@58.com>
2022-02-21 17:28:25 +0800

    feat: 利用定时器通知客户端发生了什么变化

--- websocket-demo1/websocket.js
+++ websocket-demo1/websocket.js
@@ -2,9 +2,18 @@ var ws = require('nodejs-websocket');
 console.log('开始建立连接...')
 
 ws.createServer(function (conn) {
+
+    var timer = setTimeout(() => {
+        clearTimeout(timer);
+        conn.sendText('5秒了，服务器的值发生了变化，false =》 true')
+    }, 5000)
+
     conn.on('text', function (str) {
         console.log('浏览器给服务端收到的信息为:' + str)
         conn.sendText('服务器下发的内容=>'+str)
+    })
+    conn.on('connect', function(code) {
+        console.log('开启连接', code)
     })
     conn.on('close', function (code, reason) {
         console.log('关闭连接', code, reason)

da4b4810077d947988862f2c3334b055ee9fe808
yujinhongmm <1347991226@qq.com>
2022-03-14 23:53:51 +0800

    ”实现new“

---
+++ 实现new.js
@@ -0,0 +1,10 @@
+function create(Con, ...args) {
+    // 1.新建一个新的对象
+    const obj = {};
+    // 2.将新对象的原型指针__proto__指向构造函数的原型
+    Object.setPrototypeOf(obj, Con.prototype); // 等同于obj.__proto__ = Con.prototype;
+    // 3.改变构造函数的上下文this，并将剩余参数传入
+    const result = Con.apply(obj,args);
+    // 4.判断构造函数有没有返回值，有返回值返回返回值，没有返回新对象
+    return result instanceof Object ? result : obj;
+}

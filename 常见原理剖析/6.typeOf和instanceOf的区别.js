9948fac1227bf6b93f75d4fb70c6ce07aae393e3
yujinhong01 <yujinhong01@58.com>
2022-03-28 21:03:25 +0800

    feat: 还没写完

---
+++ 常见原理剖析/6.typeOf和instanceOf的区别.js
@@ -0,0 +1,36 @@
+// typeof和instanceOf的区别
+
+// 1）typeof 6中 typeof null === 'object'
+// 2) [] {} reg function
+
+
+// typeof 不能准确的返回对象的具体类型
+console.log(typeof []);
+console.log(typeof new RegExp('/A/'));
+console.log(typeof function () {});
+console.log(typeof null);
+
+// 缺点只能检测当前已经存在的类型，不能检验自定义类型
+console.log(Object.prototype.toString.call(function () {}))
+console.log(Object.prototype.toString.call(null))
+
+
+class A {}
+// inastanceof
+console.log([] instanceof Array);
+console.log([] instanceof Object);
+const a = new A();
+console.log(a instanceof A);
+
+// 利用__proto__
+console.log([].__proto__ === Array.prototype);
+console.log([].__proto__.__proto__ === Object.prototype);
+
+
+// 自己实现instanceof
+function instanceOf(A, B) {
+    bPrototype = B.prototype;
+    while (1) {
+        if (A)
+    }
+}
